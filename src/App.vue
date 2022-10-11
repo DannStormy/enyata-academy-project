b3c89395c623dce902f14c62af6583af55c15b51
dann <dillydot22@gmail.com>
2022-10-11 17:01:37 +0100

    landing-page css

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,9 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
 #app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: 'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
