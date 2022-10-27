e66f016c6f38caf9b46ed042c2fea778267fd4cd
nwochachioma <nwochachioma1998@gmail.com>
2022-10-27 01:09:22 +0100

    removed bootstrap imports

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
 
 
 const app = createApp(App);
